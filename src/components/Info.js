54bac7a15142f029fc77d25c8752dbe773ec78b1
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-09-13 20:22:57 +0300

    Made the info component

---
+++ src/components/Info.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faB } from '@fortawesome/free-solid-svg-icons';
+
+const Info = () => {
+  return (
+    <div className="info">
+      <img src={require("../assets/images/personal-photo.jpg")} alt="Personal" className="info--personal-photo"/>
+      <h1 className="info--name">Smoleac Mihai</h1>
+      <p className="info--proffesion">Frontend Developer</p>
+      <div className="center-box">
+        <a href="myportfolio-69833.web.app" className="info--website">myportfolio.com</a>
+      </div>
+      <div className="info--buttons">
+        <a className="info--button-email">
+          <img src={require("../assets/images/button_icons/mail.png")}/>
+          Email
+        </a>
+        <a className="info--button-linkedin">
+          <img src={require("../assets/images/button_icons/linkedin.png")}/>
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Info;
